d722c9e4213c6f21e8af69867f8c02faa9099d23
Viktorius <barbier.31.viktor@gmail.com>
2025-12-27 10:50:46 +0100

    Update config.lua

--- dev-tablet/config.lua
+++ dev-tablet/config.lua
@@ -2,6 +2,7 @@ Config = {}
 
 -- Zet hier je license / discord id
 Config.AllowedIdentifiers = {
-    "license:b51f8da9245b34b2a12444a5691e7878e1ac8863",
-    "discord:1137761500489068714"
+    "license:[Zet hier u FiveM license]",
+    "discord:[Zet hier u discord id]"
 }
+
